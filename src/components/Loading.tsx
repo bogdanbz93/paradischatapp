21126a71e8aa5a698f99919881fa04c50710ab28
Bogdan <bogdanbz9933@gmail.com>
2023-03-22 12:15:58 +0200

    Listing only my events
    Listing the events created by the user

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, ActivityIndicator, StyleSheet} from 'react-native';
 
+// Constants
+import {Colors} from './constants/theme';
+
 export default function Loading() {
   return (
     <View style={styles.loadingContainer}>
-      <ActivityIndicator size="large" color="#FFF" />
+      <ActivityIndicator size="large" color={Colors.primary} />
     </View>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,34 +1,94 @@
-import React, {useContext} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-import {Title} from 'react-native-paper';
-import FormButton from '../components/FormButton';
+import React, {useState, useEffect, useContext} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {List, Divider, Title} from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import Loading from '../components/Loading';
+
+// Constants
 import {Colors} from '../components/constants/theme';
+
 // Auth statements
 import {AuthContext} from '../navigation/AuthProvider';
 
-export default function HomeScreen({navigation}: any) {
-  const {user, logout} = useContext(AuthContext);
+export default function HomeScreen() {
+  const {user} = useContext(AuthContext);
+  const [events, setEvents] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('EVENTS')
+      .where('user', '==', user.uid)
+      .onSnapshot(querySnapshot => {
+        const rooms = querySnapshot.docs.map(documentSnapshot => {
+          return {
+            _id: documentSnapshot.id,
+            // give defaults
+            name: '',
+            ...documentSnapshot.data(),
+          };
+        });
+
+        setEvents(rooms);
+
+        if (loading) {
+          setLoading(false);
+        }
+      });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
-    <View style={styles.container}>
-      <Title style={styles.title}>Evenimente listate:</Title>
-      <Text style={{marginBottom: 20}}>{user.uid}</Text>
-    </View>
+    <>
+      {/* Events list */}
+      <View style={styles.container}>
+        <Title style={styles.title}>Lista evenimentelor</Title>
+        <FlatList
+          data={events}
+          keyExtractor={(item: any) => item._id}
+          ItemSeparatorComponent={() => (
+            <View style={{height: 1, backgroundColor: '#efefef'}} />
+          )}
+          renderItem={({item}) => (
+            <List.Item
+              title={item.name}
+              description="Eveniment în data de"
+              titleNumberOfLines={1}
+              titleStyle={styles.listTitle}
+              descriptionStyle={styles.listDescription}
+              descriptionNumberOfLines={1}
+              left={props => <List.Icon {...props} icon="message" />}
+            />
+          )}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   title: {
     color: Colors.black,
+    marginBottom: 15,
   },
-
+  listTitle: {
+    color: Colors.black,
+    fontSize: 20,
+  },
+  listDescription: {
+    color: '#888',
+    marginTop: 5,
+    fontSize: 14,
+  },
   container: {
     backgroundColor: '#f5f5f5',
     color: '#000',
     padding: 15,
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
   },
   navButtonText: {
     color: Colors.white,
